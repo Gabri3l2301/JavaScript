073c92c0d22bdf26dbb94b8f8f2cde07b5eced29
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-06-14 21:18:13 -0500

    001-variables, Terminando el primer ejercicio

--- Basic/001-variables.js
+++ Basic/001-variables.js
@@ -8,5 +8,14 @@ console.log(holaMundo)
 let holaMundo2 = "Hola Mundo 2"
 console.log(holaMundo2)
 
+holaMundo2 = "Hola Mundo Modificado"
+console.log(holaMundo2)
+
 // const
+
+const holaMundo3 = "Hola Mundo 3"
+console.log(holaMundo3)
+
+// holaMundo3 = "Hola Mundo Modificado" // Esto causará un error porque no se puede reasignar una constante
+
 // Variables in JavaScript
